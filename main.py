8326a76ed518f053f01126a4f9f49be6ad5da725
KKK-we <lapinskristers@gmail.com>
2025-03-26 14:15:41 +0200

    klases d 26.03

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from data import uztaisit_vienu_spamu
 
 app = Flask (__name__)
 
 @app.route("/")
 def index():
     return render_template("tests.html")
+
+
+@app.route("/sveiki")
+def sveiki():
+    vardi = ["Marta", "Anna", "Katrina"]
+    return render_template("sveiki.html", name=vardi, last="")
+
+@app.route("/sveiki/<vards1>/<uzvards1>")
+def sveiki_cits(vards1, uzvards1):
+    vards = [vards1]
+    uzvards = uzvards1
+    return render_template("sveiki.html", name=vards, last=uzvards)
+
+@app.route("/spams")
+def spams():
+    return render_template("spams.html")
+
+@app.route("/veidot_spamu", methods=["POST"])
+def veidot():
+    name = request.form["vārds"]
+    sex = request.form["dzimums"]
+    age = request.form["vecums"]
+    uztaisit_vienu_spamu(name, age, sex)
+    return redirect("/spams")
+
 if __name__ =='__main__':
     app.run(port=5000)
